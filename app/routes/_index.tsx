37fc7fddb02c02af02d963908faa7584e132168d
Karl Mikkelsen <196181+karlmikko@users.noreply.github.com>
2025-01-26 23:01:29 +1100

    Got some graphs! Currently showing p50 only

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,46 +1,35 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import type { MetaFunction } from '@remix-run/node';
 import { BarChart } from '@mui/x-charts/BarChart';
+import { LinePlot, LineChart } from '@mui/x-charts/LineChart';
+import { ResponsiveChartContainer } from '@mui/x-charts';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import * as zip from '@zip.js/zip.js';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { DateTime } from 'luxon';
-import {
-  min,
-  max,
-  median,
-  mean,
-  average,
-  quantile,
-  standardDeviation,
-} from 'simple-statistics';
+import { mean, min, quantile, standardDeviation } from 'simple-statistics';
+import { DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
 
-const makeStats = (data) =>
-  data.length === 0
-    ? {
-        min: data[0],
-        max: data[0],
-        median: data[0],
-        mean: data[0],
-        average: data[0],
-        q1: data[0],
-        q2: data[0],
-        q3: data[0],
-        sd: 0,
-      }
-    : {
-        min: min(data),
-        max: max(data),
-        median: median(data),
-        mean: mean(data),
-        average: average(data),
-        q1: quantile(data, 0.25),
-        q2: quantile(data, 0.5),
-        q3: quantile(data, 0.75),
-        sd: standardDeviation(data),
-      };
+// https://www.statology.org/percentile-vs-quartile-vs-quantile/
+const makeStats = (data) => ({
+  p0: data.length == 0 ? data[0] : quantile(data, 0),
+  p1: data.length == 0 ? data[0] : quantile(data, 0.01),
+  p10: data.length == 0 ? data[0] : quantile(data, 0.1),
+  p25: data.length == 0 ? data[0] : quantile(data, 0.25),
+  p50: data.length == 0 ? data[0] : quantile(data, 0.5),
+  p75: data.length == 0 ? data[0] : quantile(data, 0.75),
+  p90: data.length == 0 ? data[0] : quantile(data, 0.9),
+  p99: data.length == 0 ? data[0] : quantile(data, 0.99),
+  p100: data.length == 0 ? data[0] : quantile(data, 1),
+  mean: data.length == 0 ? data[0] : mean(data),
+  sd: data.length == 0 ? 0 : standardDeviation(data),
+});
 
 const darkTheme = createTheme({
   palette: {
@@ -72,106 +61,194 @@ const Footer = () => (
 const twoDecimalPlaces = (x) => new Number(x.toFixed(2));
 
 const groupers = {
-  day: (x) => x.calendarDate,
-  week: (x) => {
-    const dt = DateTime.fromString(x.calendarDate, 'yyyy-MM-dd');
-    return `${dt.year}-w-${dt.weekNumber}`;
-  },
-  fortnight: (x) => {
-    const dt = DateTime.fromString(x.calendarDate, 'yyyy-MM-dd');
-    return `${dt.year}-2w-${Math.ceil(dt.weekNumber / 2)}`;
-  },
-  month: (x) => {
-    const dt = DateTime.fromString(x.calendarDate, 'yyyy-MM-dd');
-    return `${dt.year}-m-${dt.month}`;
-  },
-  '3months': (x) => {
-    const dt = DateTime.fromString(x.calendarDate, 'yyyy-MM-dd');
-    return `${dt.year}-3m-${Math.ceil(dt.month / 3)}`;
-  },
-  '6months': (x) => {
-    const dt = DateTime.fromString(x.calendarDate, 'yyyy-MM-dd');
-    return `${dt.year}-6m-${Math.ceil(dt.month / 6)}`;
-  },
+  day: (dt) => `${dt.year}-${dt.month}-${dt.day}`,
+  week: (dt) => `${dt.year}-w-${dt.weekNumber}`,
+  fortnight: (dt) => `${dt.year}-2w-${Math.ceil(dt.weekNumber / 2)}`,
+  month: (dt) => `${dt.year}-m-${dt.month}`,
+  '3months': (dt) => `${dt.year}-3m-${Math.ceil(dt.month / 3)}`,
+  '6months': (dt) => `${dt.year}-6m-${Math.ceil(dt.month / 6)}`,
+  year: (dt) => `${dt.year}`,
+  all: () => 'all',
 };
 
 const percentPaths = [
+  'totalSleepSeconds',
   'deepSleepPercent',
   'lightSleepPercent',
   'remSleepPercent',
   'awakeSleepPercent',
 ];
 
-const spo2Paths = ['averageSPO2', 'averageHR', 'lowestSPO2'];
+const spo2Paths = ['averageHR', 'averageSPO2', 'lowestSPO2'];
+
+const allMetrics = [...spo2Paths, ...percentPaths.slice(1)];
+
+const groupData = (data, groupFn) => {
+  const grouped = Object.groupBy(data, (x) => groupFn(x.calendarDateTime));
+  return Object.keys(grouped).reduce((acc, key) => {
+    const group = grouped[key];
+    return {
+      ...acc,
+      [key]: {
+        key,
+        minDateTime: DateTime.fromMillis(
+          Math.min(...group.map((x) => x.calendarDateTime))
+        ),
+        maxDateTime: DateTime.fromMillis(
+          Math.max(...group.map((x) => x.calendarDateTime))
+        ),
+        data: group,
+        ...spo2Paths.reduce((res, pp) => {
+          const converted = group
+            .map((x) => x?.spo2SleepSummary?.[pp])
+            .filter((x) => x >= 0);
+          return {
+            ...res,
+            [pp]: makeStats(converted),
+          };
+        }, {}),
+        ...percentPaths.reduce((res, pp) => {
+          const converted = group.map((x) => x?.[pp]).filter((x) => x >= 0);
+          return {
+            ...res,
+            [pp]: makeStats(converted),
+          };
+        }, {}),
+      },
+    };
+  }, {});
+};
+
+const startEndDates = (data) => {
+  return [data[0].calendarDateTime, data[data.length - 1].calendarDateTime];
+};
+
+const expectedDates = (start, end, groupFn, res = []) => {
+  if (start > end) return res;
+  const groupKey = groupFn(start);
+
+  if (res.includes(groupKey)) {
+    return expectedDates(start.plus({ days: 1 }), end, groupFn, res);
+  }
+  return expectedDates(start.plus({ days: 1 }), end, groupFn, [
+    ...res,
+    groupKey,
+  ]);
+};
 
 const DataView = ({ data, reset }) => {
   const [period, setPeriod] = useState('month');
+  const [initialStart, initialEnd] = startEndDates(data);
+  const [startDate, setStartDate] = useState(initialStart);
+  const [endDate, setEndDate] = useState(initialEnd);
   const groupFn = groupers[period];
-  const groupedData = useMemo(() => {
-    const grouped = Object.groupBy(data, groupFn);
-    return Object.keys(grouped).reduce((acc, key) => {
-      const group = grouped[key];
-      return {
-        ...acc,
-        [key]: {
-          data: group,
-          ...spo2Paths.reduce((res, pp) => {
-            const converted = group
-              .map((x) => x?.spo2SleepSummary?.[pp])
-              .filter((x) => x >= 0);
-            return {
-              ...res,
-              [pp]: makeStats(converted),
-            };
-          }, {}),
-          ...percentPaths.reduce((res, pp) => {
-            const converted = group.map((x) => x?.[pp]).filter((x) => x >= 0);
-            return {
-              ...res,
-              [pp]: makeStats(converted),
-            };
-          }, {}),
-        },
-      };
-    }, {});
-  }, [data, groupFn]);
+  const groupedData = groupData(
+    data.filter(
+      (x) => x.calendarDateTime >= startDate && x.calendarDateTime <= endDate
+    ),
+    groupFn
+  );
+  const rangeKeys = expectedDates(startDate, endDate, groupFn);
+  const seriesData = allMetrics.map((metricKey) => ({
+    type: 'line',
+    data: rangeKeys.map((key) => groupedData[key]?.[metricKey]?.p50),
+    label: metricKey,
+  }));
+
+  console.log(seriesData);
+
   return (
-    <div className="flex h-screen flex-col gap-10">
-      <header className="flex flex-row">
-        <div className="flex-auto grid justify-items-start">
-          <ButtonGroup variant="outlined">
-            <Button variant="text" disabled>
-              Group By
+    <LocalizationProvider dateAdapter={AdapterLuxon}>
+      <div className="flex h-screen flex-col gap-10">
+        <header className="flex flex-row">
+          <div className="flex-auto grid justify-items-start gap-5">
+            <ButtonGroup variant="outlined">
+              <Button variant="text" disabled>
+                Group By
+              </Button>
+              <>
+                {[
+                  ['day', 'Day'],
+                  ['week', 'Week'],
+                  ['fortnight', 'Fortnight'],
+                  ['month', 'Month'],
+                  ['3months', '3 Months'],
+                  ['6months', '6 Months'],
+                  ['year', 'Year'],
+                  ['all', 'All'],
+                ].map(([val, label]) => (
+                  <Button
+                    key={val}
+                    onClick={() => setPeriod(val)}
+                    variant={period === val ? 'contained' : undefined}
+                  >
+                    {label}
+                  </Button>
+                ))}
+              </>
+            </ButtonGroup>
+            <div>
+              <Button variant="text" disabled>
+                Date Range
+              </Button>
+              <DatePicker
+                label="Start Date"
+                value={startDate}
+                minDate={initialStart}
+                maxDate={endDate}
+                onChange={(v) => setStartDate(v)}
+              />
+              <DatePicker
+                label="End Date"
+                value={endDate}
+                minDate={startDate}
+                maxDate={initialEnd}
+                onChange={(v) => setEndDate(v)}
+              />
+              <Button
+                variant="text"
+                onClick={() => {
+                  setStartDate(initialStart);
+                  setEndDate(initialEnd);
+                }}
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+          <div className="w-100 h-10 flex-initial grid justify-items-end gap-5">
+            <Button variant="outlined" onClick={reset}>
+              Back
             </Button>
-            <>
-              {[
-                ['day', 'Day'],
-                ['week', 'Week'],
-                ['fortnight', 'Fortnight'],
-                ['month', 'Month'],
-                ['3months', '3 Months'],
-                ['6months', '6 Months'],
-              ].map(([val, label]) => (
-                <Button
-                  key={val}
-                  onClick={() => setPeriod(val)}
-                  variant={period === val ? 'contained' : undefined}
-                >
-                  {label}
-                </Button>
-              ))}
-            </>
-          </ButtonGroup>
-        </div>
-        <div className="w-100 h-10 flex-initial grid justify-items-end">
-          <Button variant="outlined" onClick={reset}>
-            Back
-          </Button>
-        </div>
-      </header>
-      <pre>{JSON.stringify(Object.values(groupedData), null, 2)}</pre>
-      <Footer />
-    </div>
+          </div>
+        </header>
+        <LineChart
+          series={seriesData}
+          xAxis={allMetrics.map((metricKey) => ({
+            data: rangeKeys,
+            scaleType: 'point',
+            id: 'x-axis-id',
+          }))}
+          height={800}
+        />
+        {/* <LinePlot />
+          <ChartsXAxis label="Time" position="bottom" axisId="x-axis-id" />
+          <ChartsYAxis label="Percent" position="left" />
+        </LineChart> */}
+        {/* <pre>
+          {JSON.stringify(
+            allMetrics.map((metricKey) => ({
+              data: rangeKeys,
+              scaleType: 'band',
+            })),
+            null,
+            2
+          )}
+        </pre> */}
+        {/* <pre>{JSON.stringify(Object.values(groupedData), null, 2)}</pre> */}
+        <Footer />
+      </div>
+    </LocalizationProvider>
   );
 };
 
@@ -308,7 +385,14 @@ export default function Index() {
                           (a, b) =>
                             new Date(a.calendarDate).getTime() -
                             new Date(b.calendarDate).getTime()
-                        );
+                        )
+                        .map((x) => ({
+                          ...x,
+                          calendarDateTime: DateTime.fromString(
+                            x.calendarDate,
+                            'yyyy-MM-dd'
+                          ),
+                        }));
 
                       if (entries.length > 0) {
                         setData(entries);
